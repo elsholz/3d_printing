a4a1f1b79f8f97a097d3a2a4eb87683324281ab3
elsholz <hendrik.lankers.hl@googlemail.com>
2020-09-18 10:04:59 +0200

    fix script

--- move_files.sh
+++ move_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Move all files that are goven as arguments to /media/$USER/sdcard/3d_printing/.
 # sd card is mounted under /media/$USER/sdcard.
-echo "sudo mount /dev/mmcblk0p1 /media/$USER/sdcard"
-echo sudo cp -t /media/$USER/sdcard/3d_printing/ "$@" 
-echo "sudo sync"
-echo "sudo umount -a"
+sudo mount /dev/mmcblk0p1 /media/$USER/sdcard
+sudo cp -t /media/$USER/sdcard/3d_printing/ "$@" 
+sudo sync
+sudo umount -a
 
